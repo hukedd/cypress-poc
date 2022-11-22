1e55b5396fc6938c4d3dfea4b343160e9605a0c7
Eddie Newitt <Eddie.Newitt@hafele.co.uk>
2022-11-22 14:52:00 +0000

    Cookie waits

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,11 +3,13 @@ describe('hafele poc', () => {
   it('passes', () => {
     // first visit site, set some cookies to dismiss the nonsense popups...
     cy.visit('https://www.hafele.co.uk/en/info/services/contact-us/32254/')
-    cy.wait(50)
+    cy.wait(200)
     cy.setCookie('top_ticker_banner_hide', '1')
+    cy.wait(50)
     cy.setCookie('CustomerCountryRedirect', 'GB') // geosniffing 'is this your country?' dialog
     cy.wait(200)
-    cy.visit('https://www.hafele.co.uk/en/info/services/contact-us/32254/')
+    cy.visit('https://www.hafele.co.uk/en/info/services/32168/')
+    cy.wait(50)
     cy.get('#onetrust-accept-btn-handler').should('be.visible')
     cy.get('#onetrust-accept-btn-handler').click()
     // now we're ready to start...
