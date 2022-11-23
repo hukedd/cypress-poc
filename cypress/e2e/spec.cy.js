8315533f0af3536a93bb3f197e3d8ef59bef7d33
Eddie Newitt <Eddie.Newitt@hafele.co.uk>
2022-11-23 09:16:51 +0000

    Added waits for ropey oneweb

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -15,7 +15,7 @@ describe('hafele poc', () => {
     // now we're ready to start...
     cy.wait(200)
     // ensure login link visible
-    cy.get('#headerLoginLinkAction', { timeout: 10000 }).should('be.visible')
+    cy.get('#headerLoginLinkAction', { timeout: 20000 }).should('be.visible')
     cy.get('#headerLoginLinkAction').click()
     cy.get('[data-testid="divShopLoginForm_LoginAsJointUser_headerItemLogin"]').click()
     // login details
@@ -24,26 +24,27 @@ describe('hafele poc', () => {
     cy.get('[data-testid="FlyoutLoginEditLogin"]').type('gruntgrunt')
     cy.get('[data-testid="ajaxAccountLoginFormBtn"]').click({ force: true })
     // wait for my account to be visible (indicates we've logged in OK)
-    cy.get('[data-testid="HeaderLinkMyAccount"]').should('be.visible')
+    cy.get('[data-testid="HeaderLinkMyAccount"]', { timeout: 20000 }).should('be.visible')
     // now we can click on cart link
-    cy.get('#js-cart').should('be.visible').click()
+    cy.get('#js-cart', { timeout: 20000 }).should('be.visible').click()
     // cy.get('#cart-item-value > .hidden-md').should('be.visible').click({ force: true })
     // wait for cart elements to be available & then enter item details
     cy.get('[data-testid="CartDirectOrderInputArticle"]').should('be.visible')
     cy.get('[data-testid="CartDirectOrderInputArticle"]').type('13439020')
     cy.get('[data-testid="CartDirectOrderInputQuantity"]').type('1')
-    cy.get('[data-testid="CartDirectOrderButtonAdd"]').should('be.visible')
+    cy.get('[data-testid="CartDirectOrderButtonAdd"]', { timeout: 20000 }).should('be.visible')
     cy.wait(200)
     cy.get('[data-testid="CartDirectOrderButtonAdd"]').click()
     cy.wait(500)
-    cy.get('[data-testid="OrderlineArticleNo_13439020"]', { timeout: 10000 }).should('be.visible')
+    cy.get('[data-testid="OrderlineArticleNo_13439020"]', { timeout: 20000 }).should('be.visible')
     // custom command to add csrf token to all forms on page
     // use this whenever submitting a form in this hafele.co.uk site
     cy.add_synctoken()
     cy.wait(20)
-    cy.get('[data-testid="divselectAllTop"]').should('be.visible').click({ force: true })
+    cy.get('[data-testid="divselectAllTop"]').should('be.visible', { timeout: 20000 }).click({ force: true })
     cy.wait(100)
     cy.get('[data-testid="CartDeleteSelectedItems"]').click({ force: true })
+    cy.wait(200)
     cy.get('.shoppingCartHeader').find('strong').first().should('have.text', 'Your basket is empty.')
     cy.get(".shoppingCartHeader").find('strong').first().invoke('text').then((text) => {
       expect(text.trim()).equal('Your basket is empty.')
